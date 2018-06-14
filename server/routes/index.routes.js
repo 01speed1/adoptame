bfea1df5e7da073d52c1a9e6816960b5dfc92fec
01speed1 <o1speed1@hotmail.com>
2018-06-13 23:13:06 -0500

    Vaccinations: Model and CRUD 100%

--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
 
 
-// - api/breed
+// - api/
 router.use('/master/type-animal', require('./master_detail/animalType-route'));
 router.use('/master/breed', require('./master_detail/breed-route'));
+router.use('/master/vaccination', require('./master_detail/vaccination-route'));
+
+
+
 
 module.exports = router;
