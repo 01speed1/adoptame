2bfa2d407f918b46edbe6f15bd792d2817c454f3
01speed1 <o1speed1@hotmail.com>
2018-06-25 18:38:45 -0500

    Donation Money - Model, Controller, Router 100%

--- server/app.js
+++ server/app.js
@@ -24,6 +24,7 @@ const rhRoutes       = require('./routes/master_detail/types-rhs-route');
 const donationFoodRoutes  = require('./routes/donation_types/donation-food-route');
 const donationBloodRoutes  = require('./routes/donation_types/donation-blood-route');
 const donationObjectRoutes  = require('./routes/donation_types/donation-object-route');
+const donationMoneyRoutes  = require('./routes/donation_types/donation-money-route');
 
 const userRoutes     = require('./routes/user-route');
 const animalRoutes   = require('./routes/animal-route');
@@ -61,6 +62,7 @@ app.use('/register/animals', animalRoutes);
 app.use('/donation/type-food', donationFoodRoutes);
 app.use('/donation/type-blood', donationBloodRoutes);
 app.use('/donation/type-object', donationObjectRoutes);
+app.use('/donation/type-money', donationMoneyRoutes);
 
 app.get('/', (req, res)=> { res.send('Ok in index'); })
 
