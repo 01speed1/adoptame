cf66bc88c7513e242d7f0779c41deecb0830decd
01speed1 <o1speed1@hotmail.com>
2018-06-27 15:17:53 -0500

    Donatio Money 100%

--- server/models/donation_types/donation-money-model.js
+++ server/models/donation_types/donation-money-model.js
@@ -4,6 +4,6 @@ const Schema   = mongoose.Schema;
 var donationMoneySchema = new Schema({
     quantity:       { type: Number , required: true },
     simbol:         { type: String, required: true, enum: ['Pesos', 'Dolares', 'Euros']}
-}, { collection: 'donation-money'}) 
+}, { collection: 'donation-money'})  
 
 module.exports = mongoose.model('DonationMoney', donationMoneySchema)
